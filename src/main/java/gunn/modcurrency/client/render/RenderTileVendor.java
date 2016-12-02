bce1cec10d379a384b26cb45023c0856773906cc
BeardlessBrady <beardlessbrady@gmail.com>
2016-12-02 00:00:04 -0500

    Updated License in code

--- src/main/java/gunn/modcurrency/client/render/RenderTileVendor.java
+++ src/main/java/gunn/modcurrency/client/render/RenderTileVendor.java
@@ -7,11 +7,21 @@ import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 
 /**
- * This class was created by <Brady Gunn>.
  * Distributed with the Currency-Mod for Minecraft.
+ * Copyright (C) 2016  Brady Gunn
  *
- * The Currency-Mod is open source and distributed under a
- * Custom License: https://github.com/BeardlessBrady/Currency-Mod/blob/master/LICENSE
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * File Created on 2016-11-21.
  */
@@ -20,12 +30,22 @@ public class RenderTileVendor extends TileEntitySpecialRenderer<TileVendor>{
     //Currently not in use, trying some other methods (that are more efficient, less lag) Due for V1.1.0
     @Override
     public void renderTileEntityAt(TileVendor te, double x, double y, double z, float partialTicks, int destroyStage) {
-        GlStateManager.pushMatrix();
-        GlStateManager.disableLighting();
-        
-        int facing = te.getFaceData(); //0 = North, 1 = East, 2 = South, 3 = West
+    }
 
-        //Ensures starting position is same for every facing direction
+    public void renderItems(TileVendor te, double x, double y, double z, float partialTicks, int destroyStage) {
+
+    }
+
+
+    
+
+}
+
+
+/*
+int facing = te.getFaceData(); //0 = North, 1 = East, 2 = South, 3 = West
+
+//Ensures starting position is same for every facing direction
         switch(facing){
             case 0:
                 GlStateManager.translate(x,y,z);
@@ -42,39 +62,9 @@ public class RenderTileVendor extends TileEntitySpecialRenderer<TileVendor>{
                 GlStateManager.rotate(90,0,1,0);
                 break;
         }
+ */
 
-        //West North == +
-        //South East == -
-        //renderItems(te,x,y,z,partialTicks,destroyStage);
-        
-        
-        GlStateManager.popMatrix();
-    }
-    
-    
-    //Old Renderer, Laggy as fuck 
-    public void renderItems(TileVendor te, double x, double y, double z, float partialTicks, int destroyStage) {
-        Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+//West North == +
+//South East == -
+//renderItems(te,x,y,z,partialTicks,destroyStage);
 
-        Double xStart = -2.9;
-        Double yStart = 5.6;
-        Double xTrans = 0.45;
-        Double yTrans = -0.8;
-
-      /*  for (int column = 0; column < 6; column++) {
-            for (int row = 0; row < 5; row++) {
-                for(int amnt = 0; amnt < 10; amnt++) {
-                    GlStateManager.pushMatrix();
-                    GlStateManager.scale(0.30, 0.30, 0.30);
-                    GlStateManager.translate(xStart + (xTrans * row), yStart + (yTrans * column), -0.5 - (0.2 * amnt ));
-
-                    Minecraft.getMinecraft().getRenderItem().renderItem(new ItemStack(Items.APPLE), ItemCameraTransforms.TransformType.GROUND);
-                    GlStateManager.popMatrix();
-                }
-            }
-        }
-        */
-    }
-    
-
-}
