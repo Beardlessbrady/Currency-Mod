23f62f9d58cb46662b435bcb077fbd8a9b8e83e6
BeardlessBrady <beardlessbrady@gmail.com>
2017-01-17 12:39:45 -0500

    GuiWallet now shows walletInventory slots and can be changed from 1-4 rows of slots

--- src/main/java/gunn/modcurrency/client/guis/GuiWallet.java
+++ src/main/java/gunn/modcurrency/client/guis/GuiWallet.java
@@ -1,6 +1,7 @@
 package gunn.modcurrency.client.guis;
 
 import gunn.modcurrency.common.containers.ContainerWallet;
+import gunn.modcurrency.common.items.ItemWallet;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
@@ -27,9 +28,8 @@ import net.minecraft.util.ResourceLocation;
  */
 public class GuiWallet extends GuiContainer{
     private static final ResourceLocation BACKGROUND_TEXTURE = new ResourceLocation("modcurrency", "textures/gui/GuiWalletTexture.png");
+    private static final ResourceLocation EXTRA_TEXTURE = new ResourceLocation("modcurrency", "textures/gui/GuiWalletExtraTexture.png");
     public static final int GUI_XPOS_OFFPUT = -19;
-
-
 
     public GuiWallet(InventoryPlayer invPlayer) {
         super(new ContainerWallet(invPlayer));
@@ -38,6 +38,24 @@ public class GuiWallet extends GuiContainer{
     @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
         Minecraft.getMinecraft().getTextureManager().bindTexture(BACKGROUND_TEXTURE);
-        drawTexturedModalRect(guiLeft - 19, guiTop,0 ,0 , 213, 201);
+        //Background
+        drawTexturedModalRect(guiLeft + GUI_XPOS_OFFPUT, guiTop,0 ,0 , 213, 201);
+
+        Minecraft.getMinecraft().getTextureManager().bindTexture(EXTRA_TEXTURE);
+        switch(ItemWallet.WALLET_ROW_COUNT){
+            default:
+            case 4:     //4 Rows
+                drawTexturedModalRect(guiLeft + GUI_XPOS_OFFPUT + 26, guiTop + 70,0 ,152 ,162 , 18);
+            case 3:     //3 Rows
+                drawTexturedModalRect(guiLeft + GUI_XPOS_OFFPUT + 26, guiTop + 16,0 ,98 ,162 , 18);
+            case 2:     //2 Rows
+                drawTexturedModalRect(guiLeft + GUI_XPOS_OFFPUT + 26, guiTop + 52,0 ,134 ,162 , 18);
+            case 0:
+            case 1:     //1 Row
+                drawTexturedModalRect(guiLeft + GUI_XPOS_OFFPUT + 26, guiTop + 34,0 ,116 ,162 , 18);
+        }
+
     }
+
+
 }
