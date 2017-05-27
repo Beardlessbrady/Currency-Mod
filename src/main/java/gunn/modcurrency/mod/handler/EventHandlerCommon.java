c8b908ac4b55263fcfca6957c38211bb2099f7fc
BeardlessBrady <beardlessbrady@gmail.com>
2017-05-27 17:16:31 -0400

    Forgot to turn invincibility back on

--- src/main/java/gunn/modcurrency/mod/handler/EventHandlerCommon.java
+++ src/main/java/gunn/modcurrency/mod/handler/EventHandlerCommon.java
@@ -1,6 +1,8 @@
 package gunn.modcurrency.mod.handler;
 
 import gunn.modcurrency.mod.ModConfig;
+import gunn.modcurrency.mod.block.ModBlocks;
+import gunn.modcurrency.mod.tileentity.IOwnable;
 import net.minecraft.block.Block;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -20,14 +22,13 @@ public class EventHandlerCommon {
 
         //Invincible Machines
         if (ModConfig.invincibleVendSell) {
-          /*  cancelEventChange:
-            if (brokeBlock == ModBlocks.blockSeller || brokeBlock == ModBlocks.blockVendor || brokeBlock == ModBlocks.blockTop || brokeBlock == ModBlocks.blockATM) {
+            cancelEventChange:
+            if (brokeBlock == ModBlocks.blockVending || brokeBlock == ModBlocks.blockATM) {
                 IOwnable tile = (IOwnable) e.getWorld().getTileEntity(e.getPos());
-                if (brokeBlock == ModBlocks.blockTop) tile = (abAdvSell) e.getWorld().getTileEntity(e.getPos().down());
                 if ((!e.getEntityPlayer().getUniqueID().toString().equals(tile.getOwner())) && !e.getEntityPlayer().isCreative()) {     //If not Owner (and not in creative) Can't Break
                     e.setCanceled(true);
                 }
-            }*/
+            }
         }
     }
 }
