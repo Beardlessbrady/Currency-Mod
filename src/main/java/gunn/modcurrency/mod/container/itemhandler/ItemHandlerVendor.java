b7b87a130a7b2378bb7d922056fa0f9c988ea5e9
BeardlessBrady <beardlessbrady@gmail.com>
2017-06-08 01:17:05 -0400

    Port: More fixes on Vendor

--- src/main/java/gunn/modcurrency/mod/container/itemhandler/ItemHandlerVendor.java
+++ src/main/java/gunn/modcurrency/mod/container/itemhandler/ItemHandlerVendor.java
@@ -3,8 +3,6 @@ package gunn.modcurrency.mod.container.itemhandler;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.ItemStackHandler;
 
-import javax.annotation.Nonnull;
-
 /**
  * Distributed with the Currency-Mod for Minecraft
  * Copyright (C) 2017  Brady Gunn
@@ -20,18 +18,20 @@ public class ItemHandlerVendor extends ItemStackHandler {
         for (int i = 0; i < ghostItems.length; i++) ghostItems[i] = false;
     }
 
-    @Nonnull
     @Override
-    public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
-        if (this.getStackInSlot(slot).getItem() == stack.getItem()) {
-            if (isGhost(slot)) {
-                setGhost(slot, false);
-                stack.stackSize--;
+    public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+        if(stack != null && this.getStackInSlot(slot) != null) {
+            if (this.getStackInSlot(slot).getItem() == stack.getItem() && this.getStackInSlot(slot).getItemDamage() == stack.getItemDamage()) {
+                if (isGhost(slot)) {
+                    setGhost(slot, false);
+                    stack.stackSize--;
+                }
+                return super.insertItem(slot, stack, simulate);
+            } else {
+                return stack;
             }
-            return super.insertItem(slot, stack, simulate);
-        } else {
-            return stack;
         }
+        return stack;
     }
 
     public boolean isGhost(int slot) {
