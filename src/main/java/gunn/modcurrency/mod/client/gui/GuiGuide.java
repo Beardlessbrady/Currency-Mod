f013d3dda79e8cd6389e94fabd8279a53eaf4034
BeardlessBrady <beardlessbrady@gmail.com>
2017-09-04 19:25:07 -0400

    Fixed an issue in the guide where there was an unused icon inplace of where the creative icon was supposed to be

--- src/main/java/gunn/modcurrency/mod/client/gui/GuiGuide.java
+++ src/main/java/gunn/modcurrency/mod/client/gui/GuiGuide.java
@@ -379,7 +379,7 @@ public class GuiGuide extends GuiScreen{
                 GL11.glTranslatef(guiLeft + 0.8F, guiTop + 0.8F, 0.8F);
                 GL11.glScalef(0.7F, 0.7F, 0.7F);
                 Minecraft.getMinecraft().getTextureManager().bindTexture(TAB_TEXTURE);
-                drawTexturedModalRect(55, 28, 236, 55, 19,17);
+                drawTexturedModalRect(55, 28, 236, 37, 19,17);
                 GL11.glPopMatrix();
 
                 Minecraft.getMinecraft().getTextureManager().bindTexture(BACKGROUND_TEXTURE);
@@ -535,7 +535,7 @@ public class GuiGuide extends GuiScreen{
                 GL11.glTranslatef(guiLeft + 0.8F, guiTop + 0.8F, 0.8F);
                 GL11.glScalef(0.7F, 0.7F, 0.7F);
                 Minecraft.getMinecraft().getTextureManager().bindTexture(TAB_TEXTURE);
-                drawTexturedModalRect(55, 51, 236, 55, 19,17);
+                drawTexturedModalRect(55, 51, 236, 37, 19,17);
                 GL11.glPopMatrix();
 
                 Minecraft.getMinecraft().getTextureManager().bindTexture(BACKGROUND_TEXTURE);
