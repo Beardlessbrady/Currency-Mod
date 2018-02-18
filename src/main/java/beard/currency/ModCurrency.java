b1bae8629825098e9e227ef28841ebfe367b1c65
beardlessbrady <beardlessbrady@gmail.com>
2018-02-17 21:51:52 -0500

    Initial Commit

--- src/main/java/beard/currency/ModCurrency.java
+++ src/main/java/beard/currency/ModCurrency.java
@@ -23,7 +23,6 @@ public class ModCurrency {
     public static final String VERSION = "1.12-1.3.0";
 
 
-
     @SidedProxy(clientSide = "beard.currency.proxy.ClientProxy", serverSide = "beard.currency.proxy.ServerProxy")
     public static CommonProxy proxy;
 
