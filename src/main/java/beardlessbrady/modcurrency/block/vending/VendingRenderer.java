05b18540b1ee54200b5b114d21bef709ba818030
beardlessbrady <beardlessbrady@gmail.com>
2019-02-20 10:34:27 -0500

    Working on Vending machine item render and it works..just a bit laggy...

--- src/main/java/beardlessbrady/modcurrency/block/vending/VendingRenderer.java
+++ src/main/java/beardlessbrady/modcurrency/block/vending/VendingRenderer.java
@@ -1,9 +1,11 @@
 package beardlessbrady.modcurrency.block.vending;
 
+import beardlessbrady.modcurrency.handler.StateHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -23,20 +25,46 @@ public class VendingRenderer extends TileEntitySpecialRenderer<TileVending> {
     public void render(TileVending te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
         if(!te.hasWorld())
             return;
+
+
+        int rotation = 0;
+        switch(this.getWorld().getBlockState(te.getPos()).getValue(StateHandler.FACING)){
+            case EAST:
+                rotation = -90;
+                break;
+            case WEST:
+                rotation = 90;
+                break;
+            case NORTH:
+                rotation = 0;
+                break;
+            case SOUTH:
+                rotation = 180;
+                break;
+        }
+
         RenderItem itemRenderer = Minecraft.getMinecraft().getRenderItem();
         ItemStack itemStack = new ItemStack(Items.APPLE);
 
         GlStateManager.pushMatrix();
-        GlStateManager.translate(x+5, y+5, z);
+        GlStateManager.translate(x + 0.913, y + 1.77, z + 0.2);
         GlStateManager.pushAttrib();
         RenderHelper.enableStandardItemLighting();
+        GlStateManager.rotate(rotation, 0f, 1f, 0f);
+        GlStateManager.scale(0.13f, 0.13f, 0.13f);
 
         for(int i = 0; i < 5; i++){
            for(int j = 0; j < 5; j++){
-                GlStateManager.translate(-1, 0, 0);
-               // itemRenderer.renderItem(itemStack, ItemCameraTransforms.TransformType.FIXED);
+               GlStateManager.translate(-0.9, 0, 0);
+               itemRenderer.renderItem(itemStack, ItemCameraTransforms.TransformType.FIXED);
+
+               for(int k = 0; k < 5; k++){
+                   GlStateManager.translate(0, 0, +0.5);
+                   itemRenderer.renderItem(itemStack, ItemCameraTransforms.TransformType.FIXED);
+               }
+               GlStateManager.translate(0, 0, -2.5);
             }
-            GlStateManager.translate(+5, -1, 0);
+            GlStateManager.translate(4.5, -2, 0);
         }
         RenderHelper.disableStandardItemLighting();
         GlStateManager.popAttrib();
