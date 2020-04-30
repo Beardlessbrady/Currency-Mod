cbf304ed775ea9b224cf090cae3c253437e97235
Beardlessbrady <github@bradygunn.ca>
2020-04-30 03:07:09 -0400

    Changed ModID so you can run both versions of the mod (for upgrading)

--- src/main/java/beardlessbrady/modcurrency/ModCurrency.java
+++ src/main/java/beardlessbrady/modcurrency/ModCurrency.java
@@ -25,7 +25,7 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod(modid = ModCurrency.MODID, name = ModCurrency.MODNAME, version = ModCurrency.VERSION)
 public class ModCurrency {
-    public static final String MODID = "modcurrency";
+    public static final String MODID = "modgoodolcurrency";
     public static final String MODNAME = "Good Ol' Currency";
     public static final String VERSION = "1.12.0-2.0.0-ALPHA-20200420";
 
