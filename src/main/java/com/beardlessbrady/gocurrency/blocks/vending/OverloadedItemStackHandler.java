8c4274c8ae99f4435113f4339194c061a90ecbca
beardlessbrady <github@bradygunn.ca>
2021-07-25 07:50:10 -0400

    Forgot to push some new files

---
+++ src/main/java/com/beardlessbrady/gocurrency/blocks/vending/OverloadedItemStackHandler.java
@@ -0,0 +1,248 @@
+package com.beardlessbrady.gocurrency.blocks.vending;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemHandlerHelper;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Created by BeardlessBrady on 2021-07-24 for Currency-Mod
+ * All Rights Reserved
+ * https://github.com/Beardlessbrady/Currency-Mod
+ */
+public class OverloadedItemStackHandler implements IItemHandler, IItemHandlerModifiable, INBTSerializable<CompoundNBT> {
+    protected NonNullList<ItemStack> stacks;
+    protected NonNullList<Integer> stackSize;
+    protected int stackSizeLimit = 256;
+
+    public OverloadedItemStackHandler() {
+        this(1);
+    }
+
+    public OverloadedItemStackHandler(int size) {
+        stacks = NonNullList.withSize(size, ItemStack.EMPTY);
+        stackSize = NonNullList.withSize(size, 0);
+    }
+
+    public OverloadedItemStackHandler(NonNullList<ItemStack> add) {
+        stacks = NonNullList.withSize(add.size(), ItemStack.EMPTY);
+        stackSize = NonNullList.withSize(add.size(), 0);
+
+        for (int i = 0; i < add.size(); i++) {
+            stackSize.set(i, add.get(i).getCount());
+            add.get(i).setCount(1);
+            stacks.set(i, add.get(i).copy());
+        }
+
+
+        this.stacks = stacks;
+    }
+
+    public void setSize(int size) {
+        stacks = NonNullList.withSize(size, ItemStack.EMPTY);
+        stackSize = NonNullList.withSize(size, 0);
+    }
+
+    @Override
+    public void setStackInSlot(int slot, @Nonnull ItemStack stack) {
+        validateSlotIndex(slot);
+
+        this.stackSize.set(slot, stack.getCount());
+
+        stack.setCount(1);
+        this.stacks.set(slot, stack);
+        onContentsChanged(slot);
+    }
+
+    @Override
+    public int getSlots() {
+        return stacks.size();
+    }
+
+    @Override
+    @Nonnull
+    public ItemStack getStackInSlot(int slot) {
+        validateSlotIndex(slot);
+        return this.stacks.get(slot);
+    }
+
+    public int getSizeInSlot(int slot){
+        return this.stackSize.get(slot);
+    }
+
+    /**
+     * Return leftover if cant grow
+     *
+     * @param slot which slot
+     * @param amnt amount to grow
+     * @return leftover
+     */
+    protected int growStackSize(int slot, int amnt) {
+        if (amnt + stackSize.get(slot) > stackSizeLimit) {
+            int leftover = (stackSize.get(slot) + amnt) - stackSizeLimit;
+            int grow = amnt - leftover;
+            stackSize.set(slot, stackSize.get(slot) + grow);
+            onContentsChanged(slot);
+            return leftover;
+
+        } else {
+            stackSize.set(slot, stackSize.get(slot) + amnt);
+            onContentsChanged(slot);
+            return 0;
+        }
+    }
+
+    @Override
+    @Nonnull
+    public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+        if (stack.isEmpty())
+            return ItemStack.EMPTY;
+
+        if (!isItemValid(slot, stack))
+            return stack;
+
+        validateSlotIndex(slot);
+
+        ItemStack existing = this.stacks.get(slot).copy();
+        existing.setCount(stackSize.get(slot));
+
+        int limit = getStackLimit(slot, stack);
+
+        if (!existing.isEmpty()) {
+            if (!ItemHandlerHelper.canItemStacksStack(stack, existing))
+                return stack;
+
+            limit -= existing.getCount();
+        }
+
+        if (limit <= 0)
+            return stack;
+
+        boolean reachedLimit = stack.getCount() > limit;
+
+        if (!simulate) {
+            if (existing.isEmpty()) {
+                this.stacks.set(slot, reachedLimit ? ItemHandlerHelper.copyStackWithSize(stack, limit) : stack);
+            } else {
+                growStackSize(slot, reachedLimit ? limit : stack.getCount());
+            }
+            onContentsChanged(slot);
+        }
+
+        return reachedLimit ? ItemHandlerHelper.copyStackWithSize(stack, stack.getCount()- limit) : ItemStack.EMPTY;
+    }
+
+    @Override
+    @Nonnull
+    public ItemStack extractItem(int slot, int amount, boolean simulate) {
+        if (amount == 0)
+            return ItemStack.EMPTY;
+
+        validateSlotIndex(slot);
+
+        ItemStack existing = this.stacks.get(slot).copy();
+        existing.setCount(this.stackSize.get(slot));
+
+        if (existing.isEmpty())
+            return ItemStack.EMPTY;
+
+        int toExtract = Math.min(amount, stackSizeLimit);
+
+        if (existing.getCount() <= toExtract) {
+            if (!simulate) {
+                this.stacks.set(slot, ItemStack.EMPTY);
+                this.stackSize.set(slot, 0);
+                onContentsChanged(slot);
+                return existing;
+            } else {
+                return existing.copy();
+            }
+        } else {
+            if (!simulate) {
+                this.stacks.set(slot, ItemHandlerHelper.copyStackWithSize(existing, 1));
+                this.stackSize.set(slot, (existing.getCount() - toExtract));
+
+                onContentsChanged(slot);
+            }
+
+            return ItemHandlerHelper.copyStackWithSize(existing, toExtract);
+        }
+    }
+
+    @Override
+    public int getSlotLimit(int slot) {
+        return stackSizeLimit;
+    }
+
+    protected int getStackLimit(int slot, @Nonnull ItemStack stack) {
+        return stackSizeLimit;
+    }
+
+    @Override
+    public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public CompoundNBT serializeNBT() {
+        ListNBT nbtTagList = new ListNBT();
+        for (int i = 0; i < stacks.size(); i++) {
+            if (!stacks.get(i).isEmpty()) {
+                CompoundNBT itemTag = new CompoundNBT();
+                itemTag.putInt("Slot", i);
+                stacks.get(i).write(itemTag);
+                nbtTagList.add(itemTag);
+            }
+        }
+        CompoundNBT nbt = new CompoundNBT();
+        nbt.put("Items", nbtTagList);
+        nbt.putInt("Size", stacks.size());
+
+
+        int[] sizeArray = new int[stackSize.size()];
+        for (int i = 0; i < sizeArray.length; i++) {
+            sizeArray[i] = stackSize.get(i);
+        }
+        nbt.putIntArray("OverloadedSize", sizeArray);
+        return nbt;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundNBT nbt) {
+        setSize(nbt.contains("Size", Constants.NBT.TAG_INT) ? nbt.getInt("Size") : stacks.size());
+        ListNBT tagList = nbt.getList("Items", Constants.NBT.TAG_COMPOUND);
+        for (int i = 0; i < tagList.size(); i++) {
+            CompoundNBT itemTags = tagList.getCompound(i);
+            int slot = itemTags.getInt("Slot");
+
+            if (slot >= 0 && slot < stacks.size()) {
+                stacks.set(slot, ItemStack.read(itemTags));
+            }
+        }
+
+        int[] sizeArray = nbt.getIntArray("OverloadedSize");
+        for (int i = 0; i < sizeArray.length; i++) {
+            stackSize.set(i, sizeArray[i]);
+        }
+        onLoad();
+    }
+
+    protected void validateSlotIndex(int slot) {
+        if (slot < 0 || slot >= stacks.size())
+            throw new RuntimeException("Slot " + slot + " not in valid range - [0," + stacks.size() + ")");
+    }
+
+    protected void onLoad() {
+
+    }
+
+    protected void onContentsChanged(int slot) {
+    }
+}
---
+++ src/main/java/com/beardlessbrady/gocurrency/blocks/vending/VendingContentsOverloaded.java
@@ -0,0 +1,157 @@
+package com.beardlessbrady.gocurrency.blocks.vending;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.ItemStackHelper;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.items.ItemHandlerHelper;
+import net.minecraftforge.items.ItemStackHandler;
+
+import java.util.function.Predicate;
+
+/**
+ * Created by BeardlessBrady on 2021-03-01 for Currency-Mod
+ * All Rights Reserved
+ * https://github.com/Beardlessbrady/Currency-Mod
+ */
+public class VendingContentsOverloaded implements IInventory {
+    protected final OverloadedItemStackHandler vendingComponentContents;
+    protected Predicate<PlayerEntity> canPlayerAccess = x-> true;
+    protected Notify markDirtyNotification = ()->{};
+    protected final Notify openInventoryNotificationLambda = ()->{};
+    protected final Notify closeInventoryNotificationLambda = ()->{};
+
+    @FunctionalInterface
+    public interface Notify {
+        void invoke();
+    }
+
+    // Server side initialization
+    VendingContentsOverloaded(int size, Predicate<PlayerEntity> canPlayerAccess, Notify notify) {
+        this.vendingComponentContents = new OverloadedItemStackHandler(size);
+        this.canPlayerAccess = canPlayerAccess;
+        this.markDirtyNotification = notify;
+    }
+
+    // Client side initialization
+    VendingContentsOverloaded(int size) {
+        this.vendingComponentContents = new OverloadedItemStackHandler(size);
+    }
+
+    // ----- Setters and Getters -----
+    public void setPlayerAccess(Predicate<PlayerEntity> canPlayerAccess){
+        this.canPlayerAccess = canPlayerAccess;
+    }
+
+    @Override
+    public boolean isUsableByPlayer(PlayerEntity player) {
+        return canPlayerAccess.test(player); // Only does anything on server side
+    }
+
+    public void setMarkDirty(Notify mark){
+        this.markDirtyNotification = mark;
+    }
+
+    @Override
+    public void markDirty() {
+        this.markDirtyNotification.invoke();
+    }
+
+    @Override
+    public void openInventory(PlayerEntity player) {
+        openInventoryNotificationLambda.invoke();
+    }
+
+    @Override
+    public void closeInventory(PlayerEntity player) {
+        closeInventoryNotificationLambda.invoke();
+    }
+
+    // ---- NBT STUFF -----
+    public CompoundNBT serializeNBT() {
+        return vendingComponentContents.serializeNBT();
+    }
+
+    public void deserializeNBT(CompoundNBT nbt){
+        vendingComponentContents.deserializeNBT(nbt);
+    }
+
+    // ---- Called by VANILLA to manipulate inventory contents
+    @Override
+    public int getSizeInventory() {
+        return vendingComponentContents.getSlots();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for(int i = 0; i < vendingComponentContents.getSlots(); i++){
+            if (! vendingComponentContents.getStackInSlot(i).isEmpty()) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int index) {
+        return vendingComponentContents.getStackInSlot(index);
+    }
+
+    public int getSizeInSlot(int index){
+        return vendingComponentContents.getSizeInSlot(index);
+    }
+
+    public void setSizeList(NonNullList<Integer> array){
+        vendingComponentContents.stackSize = array;
+    }
+
+    public NonNullList<Integer> getSizeList(){
+        return vendingComponentContents.stackSize;
+    }
+
+    @Override
+    public ItemStack decrStackSize(int index, int count) {
+        if (count < 0) throw new IllegalArgumentException("count should be >= 0:" + count);
+        return vendingComponentContents.extractItem(index, count, false);
+    }
+
+    @Override
+    public ItemStack removeStackFromSlot(int index) {
+        int maxStackSize = vendingComponentContents.getSlotLimit(index);
+        return vendingComponentContents.extractItem(index, maxStackSize, false);
+    }
+
+    @Override
+    public void setInventorySlotContents(int index, ItemStack stack) {
+        vendingComponentContents.setStackInSlot(index, stack);
+    }
+
+    public ItemStack growInventorySlotSize(int index, ItemStack stack){
+        if(ItemHandlerHelper.canItemStacksStack(stack, vendingComponentContents.stacks.get(index))){
+            int leftover = vendingComponentContents.growStackSize(index, stack.getCount());
+            if(leftover == 0){ // No leftover
+                return ItemStack.EMPTY;
+            } else {
+                ItemStack copy = stack.copy();
+                copy.setCount(leftover);
+                return copy;
+            }
+        } else {
+            return stack;
+        }
+    }
+
+    @Override
+    public void clear() {
+        for(int i = 0; i < vendingComponentContents.getSlots(); i++){
+            vendingComponentContents.setStackInSlot(i, ItemStack.EMPTY);
+        }
+    }
+
+    public int getStackSize(int slot){
+        return vendingComponentContents.stackSize.get(slot);
+    }
+
+    // ---- MY NEW MANIPULATION METHODS
+}
